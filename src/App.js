e4ba124c372318c164600224b5f048ea4801fdbc
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2022-04-27 01:15:16 +0530

    input field inserted

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ class App extends React.Component {
     this.setState({ counter: this.state.counter - 1 });
   };
 
+ 
+
   render() {
     return (
-      <div className="App">
-        hello
-        <ChildComponent counter={this.state.counter} />
-        <button onClick={this.incrementButton}>+</button>
-        <button onClick={this.decrementButton}>-</button>
-
+      <div className="App" style={{border : "1px solid black"}}>
+       <ChildComponent counter={this.state.counter}/>
+       <button style={{marginRight:"50px",fontSize:"30px", borderRadius:"10px"}} onClick={this.decrementButton}>-</button>{/* <ChildComponent counter={this.state.counter} /> */}
+       <button style={{fontSize:"30px" , borderRadius:"10px"}} onClick={this.incrementButton}>+</button>
       </div>
     );
   }
--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function ChildComponent({ counter }) {
   console.log("Props: ", counter);
-  return <div> {counter}</div>;
+  return <div>
+     <input type="text" style={{textAlign:"center"}} 
+     value={counter} onChange={(e)=>{console.log(e.target.value)}}/>
+     </div>;
 }
 
 export default ChildComponent;
