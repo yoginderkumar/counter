172ce6506191948bcc6d8e2b9d6b14082cded97f
yoginderkumar <yoginder@ekeekaran.com>
2022-04-26 18:25:45 +0530

    changed child compomnet

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ChildComponent({ counter }) {
   console.log("Props: ", counter);
-  return <div>State From a child component: {counter}</div>;
+  return <div>State From a child componentskjbdkjbdb: {counter}</div>;
 }
 
 export default ChildComponent;
