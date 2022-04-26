0f20fabf26774e57b89eddd70b69d2b7bba7bd34
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2022-04-27 00:33:29 +0530

    decrement button added

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ class App extends React.Component {
     this.setState({ counter: this.state.counter + 1 });
   };
 
+  decrementButton = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
     return (
       <div className="App">
-        Hello
+        hello
         <ChildComponent counter={this.state.counter} />
-        <button onClick={this.incrementButton}>Update</button>
+        <button onClick={this.incrementButton}>+</button>
+        <button onClick={this.decrementButton}>-</button>
+
       </div>
     );
   }
