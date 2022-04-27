8420b91883357b9a36d873d8e87356be5199a1f7
yoginderkumar <yoginder@ekeekaran.com>
2022-04-27 22:58:51 +0530

    input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 import "./App.css";
-import ChildComponent from "./ChildComponent";
-
-
-
 
 class App extends React.Component {
   state = {
@@ -13,22 +9,44 @@ class App extends React.Component {
 
   incrementButton = () => {
     this.setState({ counter: this.state.counter + 1 });
-
   };
 
   decrementButton = () => {
-    this.setState(({ counter: this.state.counter - 1 }));
-  }
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
+  handleCounterInputChange = (e) => {
+    this.setState({ counter: Number(e.target.value) });
+  };
+
+  handleKeyDownOnInput = (e) => {
+    switch (e.key) {
+      case "Enter":
+        this.decrementButton();
+        break;
+      case "Shift":
+        this.incrementButton();
+        break;
+      default:
+        return;
+    }
+  };
 
   render() {
     return (
       <div className="App">
         <div className="counter">
           <button onClick={this.decrementButton}>Decrement</button>
-          <h1 id="count" style={{color: this.state.counter > 0 ? '#6c5ce7' : this.state.counter === 0 ? 'grey' : '#d63031'}}>{this.state.counter}</h1>
+          <input
+            value={this.state.counter}
+            type="number"
+            onKeyDown={this.handleKeyDownOnInput}
+            onChange={this.handleCounterInputChange}
+          />
           <button onClick={this.incrementButton}>Increment</button>
         </div>
+        <div>Press "Shift" for Increment</div>
+        <div>Press "Enter" for Decrement</div>
       </div>
     );
   }
