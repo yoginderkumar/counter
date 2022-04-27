30770076a5612d12dfed18fc287c0fac09b6074d
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2022-04-27 15:25:18 +0530

    counter Feaures Increased

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import "./App.css";
 import ChildComponent from "./ChildComponent";
 
+
+
+
 class App extends React.Component {
   state = {
     counter: 0,
@@ -10,14 +13,43 @@ class App extends React.Component {
 
   incrementButton = () => {
     this.setState({ counter: this.state.counter + 1 });
+
   };
+
+  decrementButton = () => {
+    this.setState(({ counter: this.state.counter - 1 }))
+  }
+
+
+  colorChange = () => {
+
+    const countValue = document.getElementById('count');
+
+
+    if (this.state.counter > 0) {
+      countValue.style.color = '#6c5ce7'
+    }
+    if (this.state.counter < 0) {
+      countValue.style.color = '#d63031'
+
+    }
+
+
+  }
+
 
   render() {
     return (
       <div className="App">
-        Hello
-        <ChildComponent counter={this.state.counter} />
-        <button onClick={this.incrementButton}>Update</button>
+        {/* <ChildComponent counter={this.state.counter} /> */}
+        <div className="counter">
+          <button onClick={this.decrementButton}>Decrement</button>
+
+          <h1 id="count">{this.state.counter}</h1>
+          <button onClick={this.incrementButton}>Increment</button>
+          {this.colorChange()}
+
+        </div>
       </div>
     );
   }
