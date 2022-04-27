1bd9023f21258a9e8e06568989bb990ca7a45e58
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2022-04-27 17:39:24 +0530

    Method Updated

--- src/App.js
+++ src/App.js
@@ -17,14 +17,16 @@ class App extends React.Component {
   };
 
   decrementButton = () => {
-    this.setState(({ counter: this.state.counter - 1 }))
+    this.setState(({ counter: this.state.counter - 1 }));
   }
 
 
   colorChange = () => {
 
     const countValue = document.getElementById('count');
-
+    // if (this.state.counter === 0) {
+    //   countValue.style.color = 'red'
+    // }
 
     if (this.state.counter > 0) {
       countValue.style.color = '#6c5ce7'
@@ -45,9 +47,8 @@ class App extends React.Component {
         <div className="counter">
           <button onClick={this.decrementButton}>Decrement</button>
 
-          <h1 id="count">{this.state.counter}</h1>
+          <h1 id="count" style={{color: this.state.counter > 0 ? '#6c5ce7' : this.state.counter === 0 ? 'grey' : '#d63031'}}>{this.state.counter}</h1>
           <button onClick={this.incrementButton}>Increment</button>
-          {this.colorChange()}
 
         </div>
       </div>
