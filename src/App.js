d744716f5fa1c0aadbc538d9b8939e340d7df214
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2022-04-27 17:42:03 +0530

    removed color change method

--- src/App.js
+++ src/App.js
@@ -21,35 +21,13 @@ class App extends React.Component {
   }
 
 
-  colorChange = () => {
-
-    const countValue = document.getElementById('count');
-    // if (this.state.counter === 0) {
-    //   countValue.style.color = 'red'
-    // }
-
-    if (this.state.counter > 0) {
-      countValue.style.color = '#6c5ce7'
-    }
-    if (this.state.counter < 0) {
-      countValue.style.color = '#d63031'
-
-    }
-
-
-  }
-
-
   render() {
     return (
       <div className="App">
-        {/* <ChildComponent counter={this.state.counter} /> */}
         <div className="counter">
           <button onClick={this.decrementButton}>Decrement</button>
-
           <h1 id="count" style={{color: this.state.counter > 0 ? '#6c5ce7' : this.state.counter === 0 ? 'grey' : '#d63031'}}>{this.state.counter}</h1>
           <button onClick={this.incrementButton}>Increment</button>
-
         </div>
       </div>
     );
